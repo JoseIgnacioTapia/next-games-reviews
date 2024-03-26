6b68b7c69b0f1b5e3e429f465c931cf49cca5411
JoseIgnacioTapia <josignaciot@gmail.com>
2024-03-26 17:32:02 -0300

    Setup & First steps

---
+++ src/app/reviews/layout.tsx
@@ -0,0 +1,7 @@
+import React from "react";
+
+function ReviewsLayout({ children }: { children: React.ReactNode }) {
+  return <div>{children}</div>;
+}
+
+export default ReviewsLayout;
---
+++ src/app/reviews/stardev-valley/page.tsx
@@ -0,0 +1,20 @@
+import Heading from "@/components/Heading";
+import React from "react";
+
+function StardeValleyPage() {
+  return (
+    <>
+      <Heading>Stardew Valley</Heading>
+      <img
+        src="/images/stardew-valley.jpg"
+        alt=""
+        width="640"
+        height="360"
+        className="mb-2 rounded"
+      />
+      <p>This will be the review for Stardew Valley.</p>
+    </>
+  );
+}
+
+export default StardeValleyPage;
