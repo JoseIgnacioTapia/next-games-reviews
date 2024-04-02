54dc6799297d12a2f39644756dae2374160bb951
JoseIgnacioTapia <josignaciot@gmail.com>
2024-04-02 18:09:37 -0300

    Responsive cards

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,5 +1,5 @@
+import Link from "next/link";
 import Heading from "@/components/Heading";
-import type { ReactNode } from "react";
 
 export default function HomePage() {
   console.log("[HomePage] rendering");
@@ -7,7 +7,24 @@ export default function HomePage() {
   return (
     <>
       <Heading>Indie Gamer</Heading>
-      <p>Only the best indie games, reviewed for you.</p>
+      <p className="pb-3">Only the best indie games, reviewed for you.</p>
+      <div className="bg-white w-80 border rounded shadow hover:shadow-xl sm:w-full">
+        <Link
+          href="/reviews/stardev-valley"
+          className="flex flex-col sm:flex-row"
+        >
+          <img
+            src="/images/stardew-valley.jpg"
+            alt=""
+            width="320"
+            height="180"
+            className="rounded-t sm:rounded-l sm:rounded-r"
+          />
+          <h2 className="font-semibold font-orbitron py-1 text-center sm:px-2">
+            Stardew Valley
+          </h2>
+        </Link>
+      </div>
     </>
   );
 }
