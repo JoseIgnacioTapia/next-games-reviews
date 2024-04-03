4d0e496c39ab28cda5002486a0488dfc185ff9db
JoseIgnacioTapia <josignaciot@gmail.com>
2024-04-03 16:05:37 -0300

    Tailwind CSS Typography

--- src/app/reviews/stardev-valley/page.tsx
+++ src/app/reviews/stardev-valley/page.tsx
@@ -1,7 +1,14 @@
+import { readFile } from "node:fs/promises";
+import { marked } from "marked";
 import Heading from "@/components/Heading";
-import React from "react";
 
-function StardeValleyPage() {
+async function StardeValleyPage() {
+  const text = await readFile(
+    "./src/content/reviews/stardew-valley.md",
+    "utf8"
+  );
+  const html = marked(text);
+
   return (
     <>
       <Heading>Stardew Valley</Heading>
@@ -12,7 +19,10 @@ function StardeValleyPage() {
         height="360"
         className="mb-2 rounded"
       />
-      <p>This will be the review for Stardew Valley.</p>
+      <article
+        dangerouslySetInnerHTML={{ __html: html }}
+        className="max-w-screen-sm prose prose-slate"
+      />
     </>
   );
 }
