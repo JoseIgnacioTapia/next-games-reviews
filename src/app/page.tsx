89875c1be878e0e5f8607302c8d7fc5d288bbc6a
JoseIgnacioTapia <josignaciot@gmail.com>
2024-04-04 12:28:53 -0300

    Featured Review

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,42 +1,29 @@
 import Link from "next/link";
 import Heading from "@/components/Heading";
+import { getFeaturedReview } from "@/lib/reviews";
 
-export default function HomePage() {
+export default async function HomePage() {
+  const review = await getFeaturedReview();
+  console.log(review);
+
   return (
     <>
       <Heading>Indie Gamer</Heading>
       <p className="pb-3">Only the best indie games, reviewed for you.</p>
       <div className="bg-white w-80 border rounded shadow hover:shadow-xl sm:w-full">
         <Link
-          href="/reviews/stardew-valley"
+          href={`/reviews/${review.slug}`}
           className="flex flex-col sm:flex-row"
         >
           <img
-            src="/images/stardew-valley.jpg"
+            src={review.image}
             alt=""
             width="320"
             height="180"
             className="rounded-t sm:rounded-l sm:rounded-r"
           />
           <h2 className="font-semibold font-orbitron py-1 text-center sm:px-2">
-            Stardew Valley
-          </h2>
-        </Link>
-      </div>
-      <div className="bg-white w-80 border rounded shadow hover:shadow-xl sm:w-full">
-        <Link
-          href="/reviews/hollow-knight"
-          className="flex flex-col sm:flex-row"
-        >
-          <img
-            src="/images/hollow-knight.jpg"
-            alt=""
-            width="320"
-            height="180"
-            className="rounded-t sm:rounded-l sm:rounded-r"
-          />
-          <h2 className="font-semibold font-orbitron py-1 text-center sm:px-2">
-            Hallow Knight
+            {review.title}
           </h2>
         </Link>
       </div>
--- src/lib/reviews.ts
+++ src/lib/reviews.ts
@@ -2,6 +2,11 @@ import { readFile, readdir } from "node:fs/promises";
 import { marked } from "marked";
 import matter from "gray-matter";
 
+export async function getFeaturedReview() {
+  const reviews = await getReviews();
+  return reviews[0];
+}
+
 export async function getReview(slug: string) {
   const text = await readFile(`./src/content/reviews/${slug}.md`, "utf8");
   const {
@@ -21,12 +26,13 @@ export async function getReviews() {
     const review = await getReview(slug);
     reviews.push(review);
   }
+  reviews.sort((a, b) => b.date.localeCompare(a.date));
 
   return reviews;
 }
 
 export async function getSlugs() {
-  const files = await readdir("./content/reviews");
+  const files = await readdir("./src/content/reviews");
 
   return files
     .filter((file) => file.endsWith(".md"))
