dbddd9118ef30ab421c3ba4ffd906b24b39738a1
JoseIgnacioTapia <josignaciot@gmail.com>
2024-04-04 17:56:13 -0300

    Heroicons

--- src/components/ShareLinkButton.tsx
+++ src/components/ShareLinkButton.tsx
@@ -1,3 +1,5 @@
+import { LinkIcon } from "@heroicons/react/20/solid";
+
 function ShareLinkButton() {
   const handleClick = () => {
     console.log("Hi!");
@@ -6,8 +8,9 @@ function ShareLinkButton() {
   return (
     <button
       onClick={handleClick}
-      className="border px-2 py-1 rounded text-slate-500 text-sm hover:bg-orange-100 hover:text-slate-700"
+      className="border flex gap-1 items-center px-2 py-1 rounded text-slate-500 text-sm hover:bg-orange-100 hover:text-slate-700"
     >
+      <LinkIcon className="h-4 w-4" />
       Share Link
     </button>
   );
