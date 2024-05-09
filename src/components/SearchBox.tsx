d25ceea542c8dd634b71ea303ad6e396df93688f
JoseIgnacioTapia <josignaciot@gmail.com>
2024-05-09 18:24:24 -0300

    Solution Client-Only Rendering

--- src/components/SearchBox.tsx
+++ src/components/SearchBox.tsx
@@ -1,8 +1,12 @@
 "use client";
 
 import { Input } from "@headlessui/react";
+import { useIsClient } from "@/lib/hooks";
 
 function SearchBox() {
+  const isClient = useIsClient();
+  if (!isClient) return null;
+
   return <Input placeholder="Search..." />;
 }
 
